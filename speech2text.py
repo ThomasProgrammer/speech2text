43b8ad5b4edec0d6ca14e98b2927a67a62c5841c
Tomáš Mlynář <tom.mlynar1@gmail.com>
2023-06-26 16:11:54 +0200

    minor changes

--- speech2text.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-
-def speech2text(api_key: str, audio_file):
-    openai.api_key = api_key
-
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript["text"]
